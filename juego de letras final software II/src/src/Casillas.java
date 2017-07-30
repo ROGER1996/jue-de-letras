0d4d6cc9db364e1d5f684f113533a17842f2ab1e
ROGER1996 <tuamigooroger@gmail.com>
2017-07-30 18:57:30 -0500

    insertamos las imagenes de dibujos animados

--- juego de letras final software II/src/src/Casillas.java
+++ juego de letras final software II/src/src/Casillas.java
@@ -78,115 +78,11 @@ public class Casillas extends JFrame implements ActionListener
                 break;
                 
                 
-                case 2:                                  
-                int e = dimen;//t.getDim();
-                imagenes = new ImageIcon[e*e/2+1];
-                imagenes[0] = null;
-
-                for (int i = 1; i <= e*e/2; i++)
-                    imagenes[i] = new ImageIcon(getClass().getResource("/Imagenes/Numeros/" + i + ".gif"));
-                JPanel B = new JPanel(new GridLayout(e,e));
-                botones = new JButton[e][e];
-                for (int i = 0; i < e; i++)
-                {
-                    for (int j = 0; j < e; j++)
-                    {
-                        botones[i][j] = new JButton();
-                        botones[i][j].addActionListener(this);
-                        B.add(botones[i][j]);
-                    }
-                }
-                this.add(B,"Center");
-                break;
                 
                 
-                case 3:                                   
-                int f = dimen;//t.getDim();
-                imagenes = new ImageIcon[f*f/2+1];
-                imagenes[0] = null;
-
-                for (int i = 1; i <= f*f/2; i++)
-                    imagenes[i] = new ImageIcon(getClass().getResource("/Imagenes/Abecedario/" + i + ".jpg"));
-                JPanel C = new JPanel(new GridLayout(f,f));
-                botones = new JButton[f][f];
-                for (int i = 0; i < f; i++)
-                {
-                    for (int j = 0; j < f; j++)
-                    {
-                        botones[i][j] = new JButton();
-                        botones[i][j].addActionListener(this);
-                        C.add(botones[i][j]);
-                    }
-                }
-                this.add(C,"Center");
-                break;             
                 
                 
-                case 4:                                   
-                int g = dimen;//t.getDim();
-                imagenes = new ImageIcon[g*g/2+1];
-                imagenes[0] = null;
-
-                for (int i = 1; i <= g*g/2; i++)
-                    imagenes[i] = new ImageIcon(getClass().getResource("/Imagenes/CuerpoHumano/" + i + ".gif"));
-                JPanel D = new JPanel(new GridLayout(g,g));
-                botones = new JButton[g][g];
-                for (int i = 0; i < g; i++)
-                {
-                    for (int j = 0; j < g; j++)
-                    {
-                        botones[i][j] = new JButton();
-                        botones[i][j].addActionListener(this);
-                        D.add(botones[i][j]);
-                    }
-                }
-                this.add(D,"Center");
-                break;  
-                
-                
-                case 5:
-                int h = dimen;//t.getDim();
-                imagenes = new ImageIcon[h*h/2+1];
-                imagenes[0] = null;
-
-                for (int i = 1; i <= h*h/2; i++)
-                    imagenes[i] = new ImageIcon(getClass().getResource("/Imagenes/FrutasyVerduras/" + i + ".jpg"));
-                JPanel E = new JPanel(new GridLayout(h,h));
-                botones = new JButton[h][h];
-                for (int i = 0; i < h; i++)
-                {
-                    for (int j = 0; j < h; j++)
-                    {
-                        botones[i][j] = new JButton();
-                        botones[i][j].addActionListener(this);
-                        E.add(botones[i][j]);
-                    }
-                }
-                this.add(E,"Center");
-                break; 
-                     
-                
-                case 6:                          
-                int k = dimen;//t.getDim();
-                imagenes = new ImageIcon[k*k/2+1];
-                imagenes[0] = null;
-
-                for (int i = 1; i <= k*k/2; i++)
-                    imagenes[i] = new ImageIcon(getClass().getResource("/Imagenes/Animales/" + i + ".gif"));
-                JPanel F = new JPanel(new GridLayout(k,k));
-                botones = new JButton[k][k];
-                for (int i = 0; i < k; i++)
-                {
-                    for (int j = 0; j < k; j++)
-                    {
-                        botones[i][j] = new JButton();
-                        botones[i][j].addActionListener(this);
-                        F.add(botones[i][j]);
-                    }
-                }
-                this.add(F,"Center");
-                break; 
-  
+               
                 
                 default:
                 break;
