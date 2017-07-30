89ff8c8e75c248165795c9a43438cb25a8f7722e
ROGER1996 <tuamigooroger@gmail.com>
2017-07-30 17:43:41 -0500

    presentacion

---
+++ juego de letras final software II/src/src/Main.java
@@ -0,0 +1,19 @@
+package src;
+
+/**
+* Rivera maquera carlos joel
+ *  roger franco ortiz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+            
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaInicial().setVisible(true);
+                
+            }
+        });
+    }
+    
+}
---
+++ juego de letras final software II/src/src/VentanaInicial.java
@@ -0,0 +1,28 @@
+package src;
+
+/**
+ *
+ * Rivera maquera carlos joel
+ *  roger franco ortiz
+ */
+
+
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+
+public class VentanaInicial extends JFrame{
+    
+    //Declaración de Variables
+    private JLabel TituloInicial;
+    private JButton ImagenInicial;
+    private JMenuBar BarraDeMenu;
+    private JMenu MenuJugar,MenuOpciones,MenuAyuda,OpcionesNiveles,OpcionesCategorias;
+    private JMenuItem JugarIniciar,JugarSalir;
+    private JMenuItem Nivel1,Nivel2,Nivel3;
+    private JRadioButtonMenuItem DibujosAnimados,Numeros,Abecedario,CuerpoHumano,FrutasyVerduras,Animales;
+    private JMenuItem AcercaDe,Tutorial;
+    private int level=4,cate=1;
+    
+    
+}
